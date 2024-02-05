f56d1505301cc21bd8089693abd133e31dd82be5
Bogdan Gyrov <gyrovbogdan@gmail.com>
2024-02-05 14:05:14 +0300

    Change main color

--- resources/views/layots/master.blade.php
+++ resources/views/layots/master.blade.php
@@ -6,7 +6,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>PHPizza - @yield('title', 'Best Pizza')</title>
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    @section('headLinks')
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    @show
 </head>
 
 <body class="wrapper">
